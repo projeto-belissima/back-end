4a50065fc3de8c922399a63714617335a8a0e3f7
ViniRech <viniciusrech2007@gmail.com>
2025-06-26 17:46:06 -0300

    feat: adição de 'status' na model Funcionario #5

--- core/models/funcionario.py
+++ core/models/funcionario.py
@@ -4,11 +4,19 @@ from .user import User
 
 
 class Funcionario(models.Model):
+    class Status(models.IntegerChoices):
+        ATIVO = 0, "Ativo"
+        FERIAS = 1, "Em férias"
+        AFASTADO = 2, "Afastado"
+        TREINAMENTO = 3, "Em treinamento"
+        INATIVO = 4, "Inativo"
+
     usuario = models.ForeignKey(User, related_name="funcionarios", on_delete=models.PROTECT, verbose_name="Usuário")
     cpf = models.CharField(max_length=14, unique=True, verbose_name="CPF")
     formacao = models.CharField(max_length=100, verbose_name="Formação")
     cargo = models.CharField(max_length=100)
     setor = models.CharField(max_length=50)
+    status = models.IntegerField(choices=Status.choices, default=Status.ATIVO)
 
     def __str__(self):
         return f"{self.id} {self.usuario.name}"
