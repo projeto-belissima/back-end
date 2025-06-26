7ff9480acf2556d91792218622ee8ad14f0613e8
ViniRech <viniciusrech2007@gmail.com>
2025-06-26 18:27:11 -0300

    feat: adição de dados na model Encomenda #5

--- core/models/encomenda.py
+++ core/models/encomenda.py
@@ -14,21 +14,13 @@ class Encomenda(models.Model):
         DESCARTADO = 4, "Descartado"
 
     usuario = models.ForeignKey(User, related_name="encomendas", on_delete=models.PROTECT, verbose_name="Usuário")
-    vestido = models.ManyToManyField(Vestido, related_name="encomendas", blank=True)
+    vestido = models.ForeignKey(Vestido, related_name="encomendas", on_delete=models.PROTECT, null=True, blank=True)
     emissao = models.DateField(auto_now_add=True, verbose_name="Data de emissão")
+    retirada = models.DateField(null=True, blank=True, verbose_name="Data de retirada")
     status = models.IntegerField(choices=Status.choices, default=Status.ENCOMENDADO)
     medidas = models.ForeignKey(Medidas, related_name="encomendas", on_delete=models.PROTECT)
-    vestido_comprimento = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
-        verbose_name="Comprimento do vestido"
-    )
-    manga_comprimento = models.DecimalField(
-        max_digits=5,
-        decimal_places=2,
-        verbose_name="Comprimento da manga"
-    )
-    # tamanho = models.
+    observacoes = models.TextField(null=True, blank=True, verbose_name="Observações")
+    preco = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True, verbose_name="Preço acertado")
 
     def __str__(self):
         return f"{self.id} - {self.usuario.name} {self.emissao}"
