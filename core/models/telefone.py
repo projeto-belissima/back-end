96037008a8c86cfd6ef517760ad176c303502b21
fernandapadilha <vitoriafernandaifc@gmail.com>
2025-10-21 15:50:41 -0300

    fix: correção do telefone

--- core/models/telefone.py
+++ core/models/telefone.py
@@ -5,7 +5,7 @@ from .user import User
 
 class Telefone(models.Model):
     usuario = models.ForeignKey(User, related_name="telefones", on_delete=models.PROTECT, verbose_name="Usuário")
-    numero = models.CharField(max_length=15, verbose_name="Número")
+    numero = models.CharField(max_length=20, verbose_name="Número")
 
     def __str__(self):
         return f"{self.numero}"
