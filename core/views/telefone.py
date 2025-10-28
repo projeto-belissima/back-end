94a76bb7c8e2f24dda93baf1fa2ed8ea9f6af2ac
ViniRech <viniciusrech2007@gmail.com>
2025-10-28 15:53:09 -0300

    feat: filtro de telefone #10

--- core/views/telefone.py
+++ core/views/telefone.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Telefone
@@ -7,3 +8,5 @@ from core.serializers import TelefoneSerializer
 class TelefoneViewSet(ModelViewSet):
     queryset = Telefone.objects.all()
     serializer_class = TelefoneSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['usuario', 'numero']
